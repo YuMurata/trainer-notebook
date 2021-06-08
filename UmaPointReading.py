81df9bdd84c8c79b8a6baa9c58bd87b15c8b057e
purin52002 <purin7635@gmail.com>
2021-06-09 02:06:18 +0900

    remove trash

--- UmaPointReading.py
+++ UmaPointReading.py
@@ -1,14 +1,5 @@
-import os
 import pyocr
 import pyocr.builders
-import cv2
-from PIL import Image
-import sys
-#import ctypes
-from PIL import ImageGrab
-import ctypes
-from ctypes import wintypes
-import numpy as np
 import difflib
 
 class UmaPointReading():
@@ -18,14 +9,10 @@ class UmaPointReading():
     def __init__(self, tool):
 
         self.tool = tool
-        #self.__OpenUmaList()
 
     def setUmaList(self, uma_name_list:list):
         self.uma_name_list = uma_name_list
 
-    # def __OpenUmaList(self):
-    #     with open('uma_list.txt', 'r',encoding="utf-8_sig") as f:
-    #         self.uma_list = f.read().split("\n")
 
     def __isint(self, s):  # 整数値を表しているかどうかを判定
         try:
@@ -68,20 +55,9 @@ class UmaPointReading():
                                     lang="jpn",
                                     builder=pyocr.builders.LineBoxBuilder(tesseract_layout=6))
         uma_pt_rank = {}
-        #out = new_image
-        #no = 0
         for d in res:
-            #print(d.content)
             uma, pt = self.__ExtractUmaPt(d.content)
             if uma is not None:
                 uma_pt_rank[uma] = pt
-                #no = no + 1
-                #print(uma + ":" + str(pt) + "pt")
-            #cv2.rectangle(out, d.position[0], d.position[1], (0, 0, 255), 2)
-
-        # cv2.imshow("img",out)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-        # cv2.imwrite("result.png", out)
 
         return uma_pt_rank
