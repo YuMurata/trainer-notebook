a005e3a64a8e9b742c1e36aeef9a52ae4e59e05f
purin52002 <purin7635@gmail.com>
2021-06-09 02:11:39 +0900

    formatting

--- TeamStadiumInfoDetection.py
+++ TeamStadiumInfoDetection.py
@@ -15,9 +15,11 @@ from pathlib import Path
 from threading import Thread
 import time
 
-path=";C:\\Program Files\\Tesseract-OCR"
+
+path = ";C:\\Program Files\\Tesseract-OCR"
 os.environ['PATH'] = os.environ['PATH'] + path
 print(os.environ['PATH'])
+
 
 class State(Enum):
     NONE_ST = 0
@@ -32,85 +34,85 @@ class Event(Enum):
     READ_RANK_START_EV = 3
     READ_RANK_END_EV = 4
 
+
 class TeamStadiumInfoDetection(Thread):
 
-    #スコア読み取りモードに移行するために「スコア情報」と書いているかを確認する関数
+    # スコア読み取りモードに移行するために「スコア情報」と書いているかを確認する関数
     # 戻り値 bool
-    def __init__(self, uma_pt_list:UmaList):
+    def __init__(self):
         super().__init__(daemon=True)
         self.st = State.NONE_ST
         self.game_window_image = None
-
-        self.uma_pt_list = uma_pt_list
-
-        self.uma_list = self.uma_pt_list.getUmaList()#全てのウマ娘の名前のリスト
-
+        self.all_uma_name_list = UmaNameFileReader.Read()  # 全てのウマ娘の名前のリスト
+        self.uma_info_dict = UmaPointFileIO.Read()
         self.ocr_tool = self.GetOCRTool()
-        self.upr = UmaPointReading(self.ocr_tool)#スコア情報読み取るやつ
-        self.upr.setUmaList(self.uma_pt_list.getUmaList())
-        self.read_score = {}#スコア情報を読み取った結果
+        self.upr = UmaPointReader(
+            self.ocr_tool, self.all_uma_name_list)  # スコア情報読み取るやつ
+        self.read_score = {}  # スコア情報を読み取った結果
 
         print(self.read_score)
 
-
-        self.WindowSetting()
+        # EV_mat = [[None, ]]#状態遷移を2次元配列で作ろうとしたけどあきらめた
 
         self.is_updating = True
 
     def canReadScoreInfo(self):
-        #「スコア情報」と書かれている部分を切り出す
+        # 「スコア情報」と書かれている部分を切り出す
 
         if self.game_window_image is None:
             return False
 
         img = self.game_window_image.copy()
         print(img.size)
+
         img = img.convert(mode="L")
         img = ImageEnhance.Contrast(img).enhance(1.5)
         img = img.crop((150, 50, 240, 80))
-        #白抜き文字だから白黒反転
+        # 白抜き文字だから白黒反転
         img = img.point(lambda x: 255 if x < 200 else 0)
-        tool = pyocr.get_available_tools()[0]
-        res = tool.image_to_string(img,
-                                lang="jpn",
-                                builder=pyocr.builders.LineBoxBuilder(tesseract_layout=8))
+
+        builder = pyocr.builders.LineBoxBuilder(tesseract_layout=8)
+        res = self.ocr_tool.image_to_string(img,
+                                            lang="jpn",
+                                            builder=builder)
         if len(res) == 0:
             return False
-        scoreinfo_str = res[0].content.replace(' ','')
+
+        scoreinfo_str = res[0].content.replace(' ', '')
         print(scoreinfo_str)
 
         return scoreinfo_str == 'スコア情報'
 
-    #順位読み取りモードに移行するために「WIN」または「LOSE」と書いているかを確認する関数
-    #テンプレートマッチングを用いた
-    #戻り値bool
+    # 順位読み取りモードに移行するために「WIN」または「LOSE」と書いているかを確認する関数
+    # テンプレートマッチングを用いた
+    # 戻り値bool
     def canReadRank(self):
         if self.game_window_image is None:
             return False
 
         img = self.game_window_image.copy()
         img = self.pil2cv(img)
-        img = img[195:270,125:250]
+        img = img[195:270, 125:250]
 
         def load_image():
             resource_dir = './resource'
             image_name_list = ['win.png', 'lose.png']
 
-            def pred(image_name:str):
+            def pred(image_name: str):
                 resource_path = Path(resource_dir)/image_name
 
                 if not resource_path.exists():
-                    raise FileNotFoundException(f"can't read {str(resource_path)}")
+                    raise FileNotFoundException(
+                        f"can't read {str(resource_path)}")
                 return cv2.imread(str(resource_path))
 
             return [pred(image_name) for image_name in image_name_list]
-
 
         win_lose_img = load_image()
         method = eval('cv2.TM_SQDIFF_NORMED')
         min = 1.0
         for template in win_lose_img:
-            temp_result = cv2.matchTemplate(img,template,method)
+            temp_result = cv2.matchTemplate(img, template, method)
             min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(temp_result)
             if min > min_val:
                 min = min_val
@@ -140,18 +142,21 @@ class TeamStadiumInfoDetection(Thread):
         new_image = Image.fromarray(new_image)
         return new_image
 
-    def GetWindowRectFromName(self, TargetWindowTitle:str)-> tuple:
-        TargetWindowHandle = ctypes.windll.user32.FindWindowW(0, TargetWindowTitle)
+    def GetWindowRectFromName(self, TargetWindowTitle: str) -> tuple:
+        TargetWindowHandle = ctypes.windll.user32.FindWindowW(
+            0, TargetWindowTitle)
         if TargetWindowHandle == 0:
             return None
 
         Rectangle = ctypes.wintypes.RECT()
-        ctypes.windll.user32.GetWindowRect(TargetWindowHandle, ctypes.pointer(Rectangle))
-        return (Rectangle.left + 8, Rectangle.top, Rectangle.right - 8, Rectangle.bottom - 8)
+        ctypes.windll.user32.GetWindowRect(
+            TargetWindowHandle, ctypes.pointer(Rectangle))
+        return (Rectangle.left + 8, Rectangle.top,
+                Rectangle.right - 8, Rectangle.bottom - 8)
 
     def GetOCRTool(self):
-        #インストールしたTesseract-OCRのパスを環境変数「PATH」へ追記する。
-        #OS自体に設定してあれば以下の2行は不要
+        # インストールしたTesseract-OCRのパスを環境変数「PATH」へ追記する。
+        # OS自体に設定してあれば以下の2行は不要
         # path=';C:\\tesseract-ocr'
         # os.environ['PATH'] = os.environ['PATH'] + path
 
@@ -161,12 +166,11 @@ class TeamStadiumInfoDetection(Thread):
 
         return tools[0]
 
-    #スコア情報を読み取るための前処理
+    # スコア情報を読み取るための前処理
     def ReadScorePreProc(self, img):
 
         proc_img = img.convert(mode="L")
         proc_img = ImageEnhance.Contrast(proc_img).enhance(1.5)
-        #gray2bin(proc_img, 80)
         test_img = proc_img.point(lambda x: x if x < 80 else 255)
         width, height = test_img.size
         draw = ImageDraw.Draw(test_img)
@@ -184,6 +188,7 @@ class TeamStadiumInfoDetection(Thread):
 
         if self.canReadScoreInfo() and len(self.read_score) < 15:
             return Event.READ_SCORE_START_EV
+
         elif self.canReadRank():
             return Event.READ_RANK_START_EV
         else:
@@ -255,8 +260,10 @@ class TeamStadiumInfoDetection(Thread):
             self.game_window_image = ImageGrab.grab(rect)
 
     def OverWriteUmaListFile(self):
-        self.uma_pt_list.addUmaPt(self.read_score)
-        self.uma_pt_list.WriteUmaList()
+        for name, point in self.read_score.items():
+            self.uma_info_dict[name].AddPoint(point)
+
+        UmaPointFileIO.Write(self.uma_info_dict)
 
     def stop(self):
         self.is_updating = False
