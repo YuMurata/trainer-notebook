82a5a9705a3287b7f1541ef4d970d77319755234
purin52002 <purin7635@gmail.com>
2021-06-09 21:39:32 +0900

    draw graph

--- window/graph.py
+++ window/graph.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 from Uma import UmaInfo
@@ -15,15 +16,16 @@ class GraphWindow(tk.Toplevel):
         self.create_widgets()
 
     def update_fig(self, uma_info: UmaInfo):
-        x = np.arange(len(uma_info.points))
+        x = np.arange(len(uma_info.points))+1
         y = np.array(uma_info.points)
 
         self.ax.cla()
 
         # ax1
         self.ax.plot(x, y)
-        self.ax.set_title(uma_info.name)
+        self.ax.set_title(uma_info.name, fontname='Meiryo')
         self.ax.set_ylabel('score')
+        self.ax.get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
 
         self.canvas.draw()
 
@@ -31,6 +33,7 @@ class GraphWindow(tk.Toplevel):
         # Figure instance
         fig = plt.Figure()
         self.ax = fig.add_subplot(111)
+        self.ax.get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
 
         # Generate canvas instance, Embedding fig in root
         self.canvas = FigureCanvasTkAgg(fig, master=self)
