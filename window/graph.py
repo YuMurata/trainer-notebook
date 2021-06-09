7bc1a59ee1e7793bd83a0e2589f851045d9f7f81
purin52002 <purin7635@gmail.com>
2021-06-09 20:07:04 +0900

    make base canvas

--- window/graph.py
+++ window/graph.py
@@ -2,54 +2,36 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
+from Uma import UmaInfo
 
 
 class GraphWindow(tk.Toplevel):
     window_name = 'umauma graph'
 
-    def __init__(self, master, ):
+    def __init__(self, master):
         super().__init__(master)
         self.geometry("500x500")
         self.title(self.window_name)
         self.create_widgets()
 
-    def _make_fig(self):
-        x1 = np.linspace(0.0, 5.0)
-        y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
-        x2 = np.linspace(0.0, 3.0)
-        y2 = np.cos(2 * np.pi * x2) * np.exp(-x1)
+    def update_fig(self, uma_info: UmaInfo):
+        x = np.arange(len(uma_info.points))
+        y = np.array(uma_info.points)
 
-        # Figure instance
-        fig = plt.Figure()
+        self.ax.cla()
 
         # ax1
-        ax1 = fig.add_subplot(221)
-        ax1.plot(x1, y1)
-        ax1.set_title('line plot')
-        ax1.set_ylabel('Damped oscillation')
+        self.ax.plot(x, y)
+        self.ax.set_title(uma_info.name)
+        self.ax.set_ylabel('score')
 
-        # ax2
-        ax2 = fig.add_subplot(222)
-        ax2.scatter(x1, y1, marker='o')
-        ax2.set_title('Scatter plot')
-
-        # ax3
-        ax3 = fig.add_subplot(223)
-        ax3.plot(x2, y2)
-        ax3.set_ylabel('Damped oscillation')
-        ax3.set_xlabel('time (s)')
-
-        # ax4
-        ax4 = fig.add_subplot(224)
-        ax4.scatter(x2, y2, marker='o')
-        ax4.set_xlabel('time (s)')
-
-        return fig
+        self.canvas.draw()
 
     def create_widgets(self):
-        fig = self._make_fig()
+        # Figure instance
+        fig = plt.Figure()
+        self.ax = fig.add_subplot(111)
 
         # Generate canvas instance, Embedding fig in root
         self.canvas = FigureCanvasTkAgg(fig, master=self)
-        self.canvas.draw()
         self.canvas.get_tk_widget().pack()
