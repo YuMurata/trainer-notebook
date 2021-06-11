ebc14500d689b14fa89652cbc1f577aaccc4cf60
purin52002 <purin7635@gmail.com>
2021-06-11 20:49:42 +0900

    divide _create_widgets

--- window/graph.py
+++ window/graph.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter.constants import BOTTOM
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -13,7 +15,7 @@ class GraphWindow(tk.Toplevel):
         super().__init__(master)
         self.geometry("500x500")
         self.title(self.window_name)
-        self.create_widgets()
+        self._create_widgets()
 
     def update_fig(self, uma_info: UmaInfo):
         x = np.arange(len(uma_info.points))+1
@@ -29,12 +31,30 @@ class GraphWindow(tk.Toplevel):
 
         self.canvas.draw()
 
-    def create_widgets(self):
+    def _create_canvas(self):
+        frame = ttk.Frame(self)
         # Figure instance
         fig = plt.Figure()
         self.ax = fig.add_subplot(111)
         self.ax.get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
 
         # Generate canvas instance, Embedding fig in root
-        self.canvas = FigureCanvasTkAgg(fig, master=self)
+        self.canvas = FigureCanvasTkAgg(fig, master=frame)
         self.canvas.get_tk_widget().pack()
+
+        return frame
+
+    def _create_buttons(self):
+        frame = ttk.Frame(self)
+
+        self.line_button = ttk.Button(frame, text="line")
+        self.bar_button = ttk.Button(frame, text="bar")
+
+        self.line_button.pack(side=tk.LEFT)
+        self.bar_button.pack(side=tk.RIGHT)
+
+        return frame
+
+    def _create_widgets(self):
+        self._create_canvas().pack()
+        self._create_buttons().pack(pady=10, side=BOTTOM)
