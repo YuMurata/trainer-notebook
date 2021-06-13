764dd27e67286ecdafea892255f5c6ae4fd8fdc4
purin52002 <purin7635@gmail.com>
2021-06-13 12:29:45 +0900

    sort uma by score in regist

--- window/score.py
+++ window/score.py
@@ -20,7 +20,10 @@ class ScoreWindow(tk.Toplevel):
         for i in range(15):
             self.treeview_score.set(i, 1, '')
             self.treeview_score.set(i, 2, '')
-        for i, (name, point) in enumerate(read_score.items()):
+
+        score_list = sorted(read_score.items(),
+                            key=lambda x: x[1], reverse=True)
+        for i, (name, point) in enumerate(score_list):
             self.treeview_score.set(i, 1, name)
             self.treeview_score.set(i, 2, point)
 
