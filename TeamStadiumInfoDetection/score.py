a83aad59f3cf24ef001320688d26688fe3806b7a
purin52002 <purin7635@gmail.com>
2021-06-18 23:03:02 +0900

    いらないコードを削除

--- TeamStadiumInfoDetection/score.py
+++ TeamStadiumInfoDetection/score.py
@@ -1,20 +1,12 @@
-from typing import Tuple, Dict
-from TeamStadiumInfoDetection.dispatcher import BaseDispatched, Dispatcher
+from typing import Dict
 from TeamStadiumInfoDetection.linked_reader import LinkedReader
 import os
 import pyocr
 import pyocr.builders
-import cv2
 from PIL import ImageDraw,  ImageEnhance, Image
-from enum import Enum
-from UmaPointReading import UmaPointReader
 from Uma import UmaNameFileReader, UmaPointFileIO
-from exception import FileNotFoundException, InvalidTypeException
-from pathlib import Path
-from threading import Thread, RLock
+from exception import FileNotFoundException
 import time
-from snip import ImageSnipper
-from misc import pil2cv
 from logger import init_logger
 import difflib
 
@@ -24,64 +16,6 @@ logger = init_logger(__name__)
 path = ";C:\\Program Files\\Tesseract-OCR"
 os.environ['PATH'] = os.environ['PATH'] + path
 logger.debug(os.environ['PATH'])
-
-
-class State(Enum):
-    NONE_ST = 0
-    READ_SCORE_ST = 1
-    READ_RANK_ST = 2
-
-
-class Event(Enum):
-    NONE_EV = 0
-    READ_SCORE_START_EV = 1
-    READ_SCORE_END_EV = 2
-    READ_RANK_START_EV = 3
-    READ_RANK_END_EV = 4
-
-
-class ScoreDispatcher:
-    def __init__(self, callback):
-        self.init_score()
-        self.callback = callback
-
-    def update_score(self, score: dict):
-        self.current_score.update(score)
-        if self.current_score != self.old_score:
-            self.callback(self.current_score)
-
-        self.old_score = {key: value for key,
-                          value in self.current_score.items()}
-
-    def init_score(self):
-        self.current_score = dict()
-        self.old_score = dict()
-
-
-class ScoreDispatched(BaseDispatched):
-    def __init__(self, score_dict: dict) -> None:
-        super().__init__()
-        self.score_dict = score_dict.copy()
-
-    def init_item(self):
-        self.score_dict = dict()
-
-    def update_current(self, item: object):
-        if type(item) != type(self):
-            raise InvalidTypeException(f'except {str(type(self))}')
-
-        self.score_dict.update(item.score_dict)
-
-    def update_old(self, current_item: object):
-        self.score_dict = current_item.score_dict.copy()
-
-    def __ne__(self, item: object) -> bool:
-        if type(item) != type(self):
-            return False
-        return self.score_dict != item.score_dict
-
-    def copy(self):
-        return ScoreDispatched(self.score_dict)
 
 
 def _get_OCR():
@@ -142,64 +76,6 @@ class ScoreReader(LinkedReader):
 
         preproc_image = self._pre_proc(snip_image)
         return self.score_ocr.get_score(preproc_image)
-
-    def onReadRankST(self, ev):
-        if ev == Event.NONE_EV:
-            pass
-        elif ev == Event.READ_SCORE_START_EV:
-            self.st = State.READ_SCORE_ST
-        elif ev == Event.READ_SCORE_END_EV:
-            self.st = State.NONE_ST
-        elif ev == Event.READ_RANK_START_EV:
-            pass
-        elif ev == Event.READ_RANK_END_EV:
-            self.st = State.NONE_ST
-
-    def TransitionST(self):
-        ev = self.CreateEvent()
-        # print('st:'+str(self.st)+'：ev:'+str(ev))
-        if self.st == State.NONE_ST:
-            self.onNoneST(ev)
-
-        elif self.st == State.READ_SCORE_ST:
-            self.onReadScoreST(ev)
-
-        elif self.st == State.READ_RANK_ST:
-            self.onReadRankST(ev)
-
-    def OverWriteUmaListFile(self):
-        uma_info_dict = UmaPointFileIO.Read()
-
-        with self.lock:
-            for name, point in self.score_dict.items():
-                uma_info_dict[name].AddPoint(point)
-
-        UmaPointFileIO.Write(uma_info_dict)
-
-    def stop(self):
-        self.is_updating = False
-
-    def run(self):
-        while self.is_updating:
-            self.game_window_image = self.snipper.Snip()
-            if self.game_window_image:
-                self.TransitionST()
-
-            logger.debug('get lock')
-            with self.lock:
-                self.dispatcher.update_item(
-                    ScoreDispatched(self.score_dict.copy()))
-            logger.debug('release lock')
-
-            time.sleep(0.5)
-
-    def get(self):
-        logger.debug('get lock')
-        with self.lock:
-            logger.debug('copy score')
-            score_dict = self.score_dict.copy()
-        logger.debug('return')
-        return score_dict
 
 
 class ScoreOCR:
