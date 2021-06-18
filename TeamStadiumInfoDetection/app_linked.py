bc80c62d0c5347828c940510b5719ac0f45ed9cd
purin52002 <purin7635@gmail.com>
2021-06-18 23:06:33 +0900

    ファイル書き込み処理の追加

--- TeamStadiumInfoDetection/app_linked.py
+++ TeamStadiumInfoDetection/app_linked.py
@@ -9,6 +9,7 @@ from typing import Dict, Tuple
 from time import sleep
 from PIL import Image
 from copy import deepcopy
+from Uma import UmaPointFileIO
 
 logger = init_logger(__name__)
 
@@ -83,3 +84,15 @@ class AppLinkedThread(Thread):
             self.linked_dict = dict()
             self.dispatcher.update_item(
                 LinkedDispatched(self.linked_dict))
+
+    def overwrite_umainfo_file(self):
+        uma_info_dict = UmaPointFileIO.Read()
+
+        with self.lock:
+            for name, info in self.linked_dict.items():
+                score = info['score']
+                rank = info['rank']
+                uma_info_dict[name].add_score(score)
+                uma_info_dict[name].add_rank(rank)
+
+        UmaPointFileIO.Write(uma_info_dict)
