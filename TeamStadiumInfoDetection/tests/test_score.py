7a2e28f0dfa822ebc44e32256e26751d8a74044e
purin52002 <purin7635@gmail.com>
2021-06-18 21:06:03 +0900

    テスト用スクリプトを追加

---
+++ TeamStadiumInfoDetection/tests/test_score.py
@@ -0,0 +1,20 @@
+from TeamStadiumInfoDetection import ScoreReader
+from snip import ImageSnipper
+from logger import init_logger
+logger = init_logger(__name__)
+
+
+def test_read_score():
+    snipper = ImageSnipper()
+
+    def test():
+        snip_image = snipper.Snip()
+        reader = ScoreReader()
+        logger.info(f'read: {reader.can_read(snip_image)}')
+        read_dict = reader.read(snip_image)
+        logger.info(f'dict: {read_dict}')
+        logger.info(f'read_num: {len(read_dict)}')
+
+    test()
+    while input('continue? (y/n) ->') == 'y':
+        test()
