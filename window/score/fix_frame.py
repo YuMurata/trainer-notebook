54454838e9d8b7ddde79ea99ffbb583d14f81750
purin52002 <purin7635@gmail.com>
2021-07-03 03:58:58 +0900

    entryを各項目ごとにクラス化、ついでにcomboboxとか使った

--- window/score/fix_frame.py
+++ window/score/fix_frame.py
@@ -5,33 +5,113 @@ from logger import init_logger
 logger = init_logger(__name__)
 
 
+class Callback(NamedTuple):
+    fix: Callable[[Content], None]
+
+
+class RankFrame(ttk.Frame):
+    def __init__(self, master: tk.Widget):
+        super().__init__(master=master)
+
+        width = 10
+
+        label = ttk.Label(self, text='rank', width=width)
+        label.pack()
+
+        self.rank = tk.StringVar(self)
+        rank_list = list(range(1, 12+1))
+
+        def validate(P: str):
+            if not P.isdecimal():
+                return False
+            rank = int(P)
+            return rank in rank_list
+
+        val_cmd = (self.register(validate), '%P')
+        self.combo = ttk.Combobox(self, textvariable=self.rank, width=width,
+                                  values=rank_list, validatecommand=val_cmd,
+                                  validate='key')
+        self.combo.pack()
+
+    def get_text(self):
+        rank = self.rank.get()
+        return int(rank) if rank.isdecimal() else None
+
+    def set_text(self, rank: str):
+        self.rank.set(rank)
+
+
+class NameFrame(ttk.Frame):
+    def __init__(self, master: tk.Widget):
+        super().__init__(master=master)
+
+        width = 20
+
+        label = ttk.Label(self, text='name', width=width)
+        label.pack()
+
+        uma_name_list = UmaNameFileReader.Read()
+        self.name = tk.StringVar(self)
+        self.entry = ttk.Combobox(self, textvariable=self.name,
+                                  width=width, values=uma_name_list)
+        self.entry.pack()
+
+    def get_text(self):
+        return self.name.get()
+
+    def set_text(self, name: str):
+        self.name.set(name)
+
+
+class ScoreFrame(ttk.Frame):
+    def __init__(self, master: tk.Widget):
+        super().__init__(master=master)
+
+        width = 10
+
+        label = ttk.Label(self, text='score', width=width)
+        label.pack()
+
+        self.score = tk.StringVar(self)
+
+        def validate(P: str):
+            return P.isdecimal()
+
+        val_cmd = (self.register(validate), '%P')
+        self.combo = ttk.Entry(self, textvariable=self.score, width=width,
+                               validatecommand=val_cmd,
+                               validate='key')
+        self.combo.pack()
+
+    def get_text(self):
+        score = self.score.get()
+        return int(score) if score.isdecimal() else None
+
+    def set_text(self, score: str):
+        self.score.set(score)
+
+
 class FixScoreFrame(ttk.Frame):
     def __init__(self, master: tk.Widget):
         super().__init__(master=master)
 
-        option_dict = {'rank': dict(width=10),
-                       'name': dict(width=20),
-                       'score': dict(width=10), }
+        frame = ttk.Frame(self)
+        frame.pack()
 
-        key_list = list(option_dict.keys())
-        self.entry_var_dict = {key: tk.StringVar(self) for key in key_list}
 
-        entry_frame = ttk.Frame(self)
-        for key, option in option_dict.items():
-            self._create_entry(entry_frame, key, option).pack(
-                side=tk.LEFT, padx=3)
-        entry_frame.pack()
+        self.rank_frame = RankFrame(frame)
+        self.rank_frame.pack(side=tk.LEFT, padx=3)
+
+        self.name_frame = NameFrame(frame)
+        self.name_frame.pack(side=tk.LEFT, padx=3)
+
+        self.score_frame = ScoreFrame(frame)
+        self.score_frame.pack(side=tk.LEFT, padx=3)
 
         button = ttk.Button(self, text='fix')
         button.pack()
 
-    def _create_entry(self, master: tk.Widget, key: str, option: dict):
-        frame = ttk.Frame(master)
-        label = ttk.Label(frame, text=key, **option)
-        entry = ttk.Entry(
-            frame, textvariable=self.entry_var_dict[key], **option)
+        self.callback: Callback = None
 
-        label.pack()
-        entry.pack()
 
         return frame
