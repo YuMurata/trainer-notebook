eaced18891649a4e583c122747470bc1ac5d9bda
purin52002 <purin7635@gmail.com>
2021-07-03 03:56:42 +0900

    treeviewの遺物を削除

--- window/score/score.py
+++ window/score/score.py
@@ -5,6 +5,7 @@ from tkinter import ttk
 from TeamStadiumInfoDetection import Dispatcher
 from window.app import BaseApp
 from .treeview import ScoreTree
+from .treeview import ScoreTree, Content
 from .fix_frame import FixScoreFrame
 from logger import init_logger
 
@@ -14,6 +15,10 @@ logger = init_logger(__name__)
 class ScoreFrame(ttk.Frame):
     def __init__(self, master: tk.Widget, metrics_updater: Callable[[], None]):
         super().__init__(master)
+
+        self.treeview_score = ScoreTree(self)
+        self.treeview_score.bind('<<UpdateApp>>', self.update_app)
+        self.treeview_score.bind('<<TreeviewSelect>>', self._select_item)
 
         def generate_update_app():
             self.treeview_score.event_generate('<<UpdateApp>>', when='tail')
@@ -27,41 +32,8 @@ class ScoreFrame(ttk.Frame):
         self.metrics_updater = metrics_updater
         self.content_dict: Dict[str, Dict[str, int]] = dict()
 
-    def _clear_treeview(self):
-        for i in range(self.treeview_height):
-            self.treeview_score.set(i, 1, '')
-            self.treeview_score.set(i, 2, '')
-            self.treeview_score.set(i, 3, '')
-
-    def _fill_treeview(self):
-        def sort_key(x: Tuple[str, Dict[str, int]]):
-            logger.debug(f'name: {x[0]}, val: {x[1]}')
-            if 'score' in x[1]:
-                return (-x[1]['score'], x[0])
-            return (0, x[0])
-
-        content_list = sorted(self.content_dict.items(), key=sort_key)
-
-        for i, (name, content) in enumerate(content_list):
-            if 'rank' in content:
-                self.treeview_score.set(i, 1, content['rank'])
-
-            if 'score' in content:
-                self.treeview_score.set(i, 3, content['score'])
-
-            self.treeview_score.set(i, 2, name)
-
     def update_app(self, event):
         self.content_dict = self.linked_thread.get()
-
-        # tree_length = 15
-        # if len(self.content_dict) > tree_length:
-        #     logger.debug('幻の16人目')
-        #     logger.debug(self.content_dict)
-        #     return
-
-        self._clear_treeview()
-        self._fill_treeview()
 
     def destroy(self) -> None:
         self.linked_thread.stop()
@@ -70,34 +42,6 @@ class ScoreFrame(ttk.Frame):
     def deleteResultReadScore(self):
         self.linked_thread.init_dict()
         self.treeview_score.event_generate('<<UpdateApp>>', when='tail')
-
-    def _create_treeview(self):
-        frame = ttk.Frame(self)
-        frame.pack()
-
-        self.treeview_score = ttk.Treeview(
-            frame, columns=['Num', 'Rank', 'Name', 'Score'], height=self.treeview_height,
-            show="headings")
-        self.treeview_score.column('Num', width=40)
-        self.treeview_score.column('Rank', width=40)
-        self.treeview_score.column('Name', width=120)
-        self.treeview_score.column('Score', anchor='e', width=50)
-
-        # Create Heading
-        self.treeview_score.heading('Num', text='Num', anchor='center')
-        self.treeview_score.heading('Rank', text='Rank', anchor='center')
-        self.treeview_score.heading('Name', text='Name', anchor='center')
-        self.treeview_score.heading('Score', text='Score', anchor='center')
-
-        # Add data
-        for i in range(self.treeview_height):
-            self.treeview_score.insert(
-                parent='', index='end', iid=i, values=(i+1, '', ''))
-
-        self.treeview_score.bind('<<UpdateApp>>', self.update_app)
-        self.treeview_score.pack()
-
-        return frame
 
     def _create_button(self):
         frame = ttk.Frame(self)
@@ -120,13 +64,6 @@ class ScoreFrame(ttk.Frame):
     def _regist(self):
         self.linked_thread.overwrite_umainfo_file()
         self.metrics_updater()
-
-    def _create_widgets(self):
-        # self._create_treeview().pack()
-        self.treeview_score = ScoreTree(self)
-        self.treeview_score.pack()
-        FixScoreFrame(self).pack(expand=True, fill=tk.BOTH)
-        self._create_button().pack()
 
 
 class ScoreApp(BaseApp):
