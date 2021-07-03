a9f3ea756e9233f89b39f1ee8f19664d38e42090
purin52002 <purin7635@gmail.com>
2021-07-03 23:09:56 +0900

    loggerをクラス化
    init_loggerはdeplicate、そのうち消す

--- logger.py
+++ logger.py
@@ -1,21 +1,44 @@
-from logging import getLogger, StreamHandler, DEBUG, Formatter
+from logging import Logger, StreamHandler, DEBUG, Formatter
+
+
+class ScopeLogger:
+    def __init__(self, logger: Logger, title: str) -> None:
+        self.logger = logger
+        self.title = title
+
+    def __enter__(self):
+        self.logger.debug(f'--- start {self.title} ---')
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.logger.debug(f'--- end {self.title} ---')
+
+
+class CustomLogger(Logger):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+        handler = StreamHandler()
+        formatter = Formatter(
+            '- %(pathname)s, line %(lineno)d, in %(funcName)s \n'
+            '  - %(threadName)s: [%(levelname)s] %(message)s')
+
+        handler.setFormatter(formatter)
+
+        handler.setLevel(DEBUG)
+        self.setLevel(DEBUG)
+
+        self.addHandler(handler)
+
+        self.propagate = False
+
+    def scope(self, title: str) -> ScopeLogger:
+        return ScopeLogger(self, title)
 
 
 def init_logger(name: str):
-    logger = getLogger(name)
-
-    handler = StreamHandler()
-    formatter = Formatter(
-        '- %(pathname)s, line %(lineno)d, in %(funcName)s \n'
-        '  - %(threadName)s: [%(levelname)s] %(message)s')
-
-    handler.setFormatter(formatter)
-
-    handler.setLevel(DEBUG)
-    logger.setLevel(DEBUG)
-
-    logger.addHandler(handler)
-
-    logger.propagate = False
+    logger = CustomLogger(name)
+    logger.warning('init_logger is deplicate !')
+    logger.warning('use CustomLogger')
 
     return logger
