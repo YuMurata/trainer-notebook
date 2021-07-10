d80c581c5b4307b292c0120ef187c58b6403e3c6
purin52002 <purin7635@gmail.com>
2021-07-10 21:29:50 +0900

    [追加] 注意書きとか作成方法とか

--- build.py
+++ build.py
@@ -1,7 +1,28 @@
 '''
-`python main.py`を実行したときにエラーが出る場合、
-`python build.py`で生成したexeファイルを実行すると
-`failed to execute script main`と表示される（多分）
+# exe作成方法
+```
+pip install -r requirements.txt
+python build.py
+```
+
+# exe作成場所
+```
+dist/umaumadrive.exe
+```
+
+> **NOTE**
+>
+> `python main.py`を実行したときにエラーが出る場合、
+> `python build.py`で生成したexeファイルを実行すると
+> `failed to execute script main`と表示される（多分）
+
+> **NOTE**
+>
+> microsoft storeからインストールしたpythonだと動かない可能性あり
+> https://bitwalk.blogspot.com/2020/10/pyinstaller.html
+>
+> 普通？のpythonインストール方法
+> https://www.python.jp/install/windows/install.html
 '''
 import subprocess
 
